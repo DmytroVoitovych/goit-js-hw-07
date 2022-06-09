917103cae692bc673d1c014c884f0f35cd25eba1
DmytroVoitovych <begunec23@gmail.com>
2022-06-09 03:14:16 +0200

    готово

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+//добавил изображения в разметку
+const gallery = document.querySelector(".gallery");
+const imgGallery = galleryItems.map(({ preview, original, description }) => {
+return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="'${description}'"
+    />
+  </a>`;
+}).join(" ");
+gallery.insertAdjacentHTML("beforeend", ` ${imgGallery}`);
+
+//блок перехода
+const link = document.querySelectorAll('.gallery__link');
+const stopLink = (e)=> e.preventDefault();
+[...link].map(item => item.addEventListener('click', stopLink));
+
+
+//выход по ескейпу
+const escapeCloseModal = (e) => {
+    if (!e.code === 'ESCAPE') {  return; }
+    window.removeEventListener('keydown', escapeCloseModal); document.querySelector('.basicLightbox').remove();
+};
+
+
+//библиотека которая мне не нравится
+const clickImg = (evt) => {
+    window.addEventListener('keydown', escapeCloseModal);
+    if (!evt.target.classList.contains("gallery__image") ) { return; } const instance = basicLightbox.create(
+          	`<img  src="${evt.target.dataset.source}"></img>`
+)
+
+instance.show()
+}; 
+gallery.addEventListener('click', clickImg);
+
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+//добавил изображения в разметку
+const gallery = document.querySelector(".gallery");
+const imgGallery = galleryItems.map(({ preview, original, description }) => {
+return `<li><a  class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="'${description}'"
+      title='${description}'
+          
+    />
+  </a></li>`;
+}).join(" ");
+gallery.insertAdjacentHTML("beforeend", ` ${imgGallery}`);
+
+
+
+//блок перехода
+const link = document.querySelectorAll('.gallery__link');
+const stopLink = (e)=> e.preventDefault();
+[...link].map(item => item.addEventListener('click', stopLink));
+//получше преведущего
+var lightbox = new SimpleLightbox('.gallery a', {captionDelay:250});
+
+
